2e5b83b8b82fd7a3e8da8565a14f02e7876053fd
benjones13 <ben.jones@plymouth.ac.uk>
2021-03-02 15:22:44 +0000

    Class sampling function tested, debugged and working

--- R/MyCATS_ClassSampler.R
+++ R/MyCATS_ClassSampler.R
@@ -2,24 +2,22 @@ MyCATS_ClassSampler = function(file_in, file_out, seed = NULL){
   if(substr(file_in, nchar(file_in)-4, nchar(file_in)) != ".xlsx"){
     stop("Filepath in must end in .xlsx")
   }
-  if(substr(file_out, nchar(filepath_in)-4, nchar(filepath)) != ".xlsx"){
+  if(substr(file_out, nchar(file_out)-4, nchar(file_out)) != ".xlsx"){
     stop("Filepath out must end in .xlsx")
   }
   if(is.null(seed)){stop("Please use the seed argument to set the seed to ensure reproducibility")}
-
+  set.seed(seed)
   classlist = openxlsx::read.xlsx(file_in)
-
   classlist_sampled = data.frame(School.ID = rep(unique(classlist$School.ID), each = 6),
-                                 Class.ID = rep(NA, length(unique(classlist$School.ID)) * 6 ),
                                  Class.name = rep(NA, length(unique(classlist$School.ID)) * 6),
                                  Year.group = rep(NA, length(unique(classlist$School.ID)) * 6))
+  print(classlist_sampled)
   for(i in unique(classlist$School.ID)){
-    print(i)
-    recep.classes = sample(classlist$Class.ID[classlist$School.ID == i & classlist$Year.group == "reception"], 2)
-    yr1.classes = sample(classlist$Class.ID[classlist$School.ID == i & classlist$Year.group == "year 1"], 2)
-    yr2.classes = sample(classlist$Class.ID[classlist$School.ID == i & classlist$Year.group == "year 2"], 2)
-    classlist_sampled[classlist_sampled$School.ID == i,c("Class.ID", "Class.name", "Year.group")] = classlist[classlist$School.ID == i & classlist$Class.ID %in% c(recep.classes, yr1.classes, yr2.classes),
-                                                                                                              c("Class.ID","Class.name","Year.group")]
+    recep.classes = sample(classlist$Class.name[classlist$School.ID == i & classlist$Year.group %in% c("Reception", "Reception ")], 2)
+    yr1.classes = sample(classlist$Class.name[classlist$School.ID == i & classlist$Year.group == "Year 1"], 2)
+    yr2.classes = sample(classlist$Class.name[classlist$School.ID == i & classlist$Year.group == "Year 2"], 2)
+    classlist_sampled[classlist_sampled$School.ID == i,c("Class.name", "Year.group")] = classlist[classlist$School.ID == i & classlist$Class.name %in% c(recep.classes, yr1.classes, yr2.classes),
+                                                                                                              c("Class.name","Year.group")]
     rm(recep.classes, yr1.classes, yr2.classes)
 
   }
